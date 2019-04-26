772d337499dfeeaad8cec3df547ca4c4717c8329
brianChen0000 <50001902+brianChen0000@users.noreply.github.com>
2019-04-26 08:55:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  homework15
+//
+//  Created by Mac on 2019/3/29.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc,char** argv)
+{
+    int a,b;
+    cout<<"輸入小時";
+    cin>>a;
+    cout<<"輸入分鐘";
+    cin>>b;
+    if(a>7;b>30)
+    {
+        cout<<"上課時間";
+    }
+    else if(a>17)
+    {
+        cout<<"下課時間";
+    }
+    else if(a<7)
+    {
+        cout<<"下課時間";
+    }
+    else
+    {
+        cout<<"下課時間";
+    }
+}
